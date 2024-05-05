c3d7ebfb9d9224295f812d3b7edccd725c401f27
henrique1357 <159020137+henrique1357@users.noreply.github.com>
2024-05-05 14:00:38 +0900

    Add files via upload

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -1,11 +1,14 @@
 //btnというidを持つHTML要素を取得し、定数に代入する
 const addBtn = document.getElementById('btn');
 
+//textというidを持つHTML要素を取得し、定数に代入する
+const upText = document.getElementById('text');
+
 //HTML要素がクリックされた時にイベント処理を実行する
 addBtn.addEventListener('click', () => {
     //2秒の待時間を設定し、非同期処理を実行する
     setTimeout(() => {
         //textのコメント更新する
-        text.textContent = `ボタンをクリックしました`;
+        upText.textContent = `ボタンをクリックしました`;
     }, 2000);
 });
